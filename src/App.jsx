ed1b6e3f01986958ace0b2dd890108a0366ec370
Tristan Ford <tristan4d@gmail.com>
2023-12-05 17:48:04 -0800

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Tooltip,
+  Polyline,
+  ScaleControl,
+} from "react-leaflet";
+import * as L from "leaflet";
+import { haversine_distance, readFile } from "./utils";
+
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+// import jsonData from "../public/gtfs/route_shapes.json";
+// const route_shapes_data = JSON.parse(JSON.stringify(jsonData));
+// const route_shapes = Object.values(route_shapes_data["shapes"]).flat();
+
+const pathOptions = {
+  0: { color: "lime" },
+  1: { color: "darkorange" },
+  2: { color: "purple" },
+  3: { color: "blue" },
+  4: { color: "green" },
+  5: { color: "red" },
+  6: { color: "yellow" },
+};
+
+function App() {
+  const [map, setMap] = useState(null);
+  const [polylines, setPolylines] = useState([]);
+  const [files, setFiles] = useState(null);
+  const [trip_idx, setTrip_idx] = useState([]);
+  const [route_idx, setRoute_idx] = useState([]);
+  const [curr_idx, setCurr_idx] = useState({});
+  const [clinics, setClinics] = useState([]);
+  // const [center, setCenter] = useState([49.285707, -123.112084]);
+  const [center, setCenter] = useState([49.274503, -123.122183]);
+
+  //  Create the Icon
+  const LeafIcon = L.Icon.extend({
+    options: {},
+  });
+
+  const blueIcon = new LeafIcon({
+      iconUrl:
+        "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF",
+    }),
+    greenIcon = new LeafIcon({
+      iconUrl:
+        "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF",
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await readFile();
+        setFiles(result);
+        setTrip_idx(result["trips"].map((row) => row.shape_id));
+        setRoute_idx(result["routes"].map((row) => row.route_id));
+      } catch (error) {
+        console.error("Error reading file:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (map && files) {
+      const newLines = [];
+      const tripIdx = [];
+      const routeIdx = [];
+      const newClinics = [];
+
+      // console.log(Object.values(files["clinics"]).map((row) => row.LATITUDE));
+
+      Object.values(files["clinics"]).map((row) => {
+        const lat = parseFloat(row.LATITUDE);
+        const lon = parseFloat(row["LONGITUDE\r"]);
+
+        if (haversine_distance([lat, lon], center) < 1) {
+          newClinics.push([lat, lon]);
+        }
+      });
+
+      for (let shape in files["shapes"]) {
+        let closeToCenter = false;
+
+        const newLine = files["shapes"][shape].map((row) => {
+          const lat =
+            parseFloat(row.shape_pt_lat) *
+            (1 + (Math.random() - 0.5) / 2000000);
+          const lon =
+            parseFloat(row.shape_pt_lon) *
+            (1 + (Math.random() - 0.5) / 2000000);
+
+          if (!closeToCenter) {
+            closeToCenter = haversine_distance([lat, lon], center) < 0.2;
+          }
+          return [lat, lon];
+        });
+
+        if (closeToCenter) {
+          const newId = trip_idx.indexOf(shape);
+          const routeId = files["trips"][newId].route_id;
+          routeIdx.push(route_idx.indexOf(routeId));
+          tripIdx.push(newId);
+          newLines.push(newLine);
+        }
+      }
+
+      setCurr_idx({ headsign: tripIdx, route: routeIdx });
+      setPolylines(newLines);
+      setClinics(newClinics);
+    }
+  }, [map, files]);
+
+  return (
+    <div className="h-full">
+      <MapContainer
+        style={{ height: "100vh" }}
+        center={center}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        ref={setMap}
+      >
+        <ScaleControl position="topleft" />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={center}>
+          <Popup>Test Center</Popup>
+        </Marker>
+        {clinics.map((clinic, idx) => (
+          <Marker position={clinic} key={idx} icon={greenIcon}>
+            <Popup>Clinic</Popup>
+          </Marker>
+        ))}
+        {polylines.map((polyline, idx) => (
+          <Polyline
+            key={idx}
+            positions={polyline}
+            pathOptions={
+              pathOptions[files["routes"][curr_idx.route[idx]].route_type]
+            }
+          >
+            <Tooltip sticky>
+              <p className="font-bold">
+                {files["trips"][curr_idx.headsign[idx]].trip_headsign}
+              </p>
+            </Tooltip>
+          </Polyline>
+        ))}
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,88 @@
+import shapes from "../public/gtfs/shapes_sorted.txt";
+import routes from "../public/gtfs/routes.txt";
+import trips from "../public/gtfs/trips.txt";
+import clinics from "../public/hlbc_walkinclinics.txt";
+
+const processCSV = (str, delim = ",") => {
+  const headers = str.slice(0, str.indexOf("\n")).split(delim);
+  const rows = str.slice(str.indexOf("\n") + 1).split(/\r\n|\n/);
+
+  const newArray = rows.map((row) => {
+    const values = row.split(delim);
+    const eachObject = headers.reduce((obj, header, i) => {
+      obj[header] = values[i];
+      return obj;
+    }, {});
+    return eachObject;
+  });
+
+  return newArray;
+};
+
+const groupByShapeID = (array) => {
+  return array.reduce(function (r, a) {
+    const { shape_id, ...rest } = a;
+    r[shape_id] = r[shape_id] || [];
+    r[a.shape_id].push(rest);
+    return r;
+  }, Object.create(null));
+};
+
+export const readFile = async () => {
+  const files = {
+    shapes: shapes,
+    routes: routes,
+    trips: trips,
+    clinics: clinics,
+  };
+  const filesObject = {};
+
+  for (let file in files) {
+    try {
+      // Fetch the CSV file
+      const response = await fetch(files[file]);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Get the CSV text content
+      const csvText = await response.text();
+
+      const rawData = processCSV(csvText);
+      if (file === "shapes") {
+        filesObject[file] = groupByShapeID(rawData);
+      } else {
+        filesObject[file] = rawData;
+      }
+    } catch (error) {
+      console.error("Error fetching CSV:", error.message);
+    }
+  }
+
+  return filesObject;
+};
+
+function toRadians(degrees) {
+  const pi = Math.PI;
+  return degrees * (pi / 180);
+}
+
+export function haversine_distance(origin, destination) {
+  const [lat1, lon1] = origin;
+  const [lat2, lon2] = destination;
+  const radius = 6371; // earth radius in km
+
+  const dlat = toRadians(lat2 - lat1);
+  const dlon = toRadians(lon2 - lon1);
+  const a =
+    Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+    Math.cos(toRadians(lat1)) *
+      Math.cos(toRadians(lat2)) *
+      Math.sin(dlon / 2) *
+      Math.sin(dlon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = radius * c;
+
+  return d;
+}
